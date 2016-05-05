624f180d61ff7d3b6a1b71bcb68b50d702dd4a2d
edsonmm <ed@edsonmm.com>
2016-05-05 15:28:16 +0000

    Creacion de registros

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -24,7 +24,23 @@ class UsersController extends AppController
 
     public function add()
     {
-        echo "agregar usuario";
-        exit();
+        $user = $this->Users->newEntity();
+
+        if($this->request->is('post'))
+        {
+            $user = $this->Users->patchEntity($user, $this->request->data);
+
+            if($this->Users->save($user))
+            {
+                $this->Flash->success('El usuario ha sido creado correctamente.');
+                return $this->redirect(['controller' => 'Users', 'action' => 'index']);
+            }
+            else
+            {
+                $this->Flash->error('El usuario no pudo ser creado. Por favor, intente nuevamente.');
+            }
+        }
+
+        $this->set(compact('user'));
     }
 }
